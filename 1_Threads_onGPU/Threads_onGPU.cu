81963df36b20c14e4c88d0d08b35a6f7b1651136
HimachalamP <himachalam.padmaraju@gmail.com>
2026-06-30 19:36:05 +0200

    Create Threads_onGPU.cu

---
+++ 1_Threads_onGPU/Threads_onGPU.cu
@@ -0,0 +1,42 @@
+%%writefile Threads_onGPU.cu
+
+#include <cuda_runtime.h>
+#include <iostream>
+
+__global__ void addOne(int *data)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    printf("Block %d, Thread %d -> global id %d\n",
+           blockIdx.x, threadIdx.x, idx);
+
+    data[idx]++;
+}
+
+int main()
+{
+    const int N=8;
+
+    int h[N]={0,1,2,3,4,5,6,7};
+
+    int *d;
+
+    cudaMalloc(&d,N*sizeof(int));
+
+    cudaMemcpy(d,h,
+               N*sizeof(int),
+               cudaMemcpyHostToDevice);
+
+    addOne<<<2,N/2>>>(d);
+
+    cudaMemcpy(h,d,
+               N*sizeof(int),
+               cudaMemcpyDeviceToHost);
+    
+    std::cout<<"\n Result = ";
+
+    for(auto x:h)
+        std::cout<<x<<" ";
+
+    cudaFree(d);
+}
